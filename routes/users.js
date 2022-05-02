741efa26cd0d812f525089487f2af1e880077881
hoangnhut <hoangnhut@evolgroup.vn>
2022-05-02 17:51:11 +0700

    yad 01 express demo

---
+++ routes/users.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+
+let users = [
+  {id: 1, name: 'Alice', active: true},
+  {id: 2, name: 'Bob', active: true},
+  {id: 3, name: 'Charlie', active: false},
+  {id: 4, name: 'Dave', active: true},
+  {id: 5, name: 'Eve', active: true}
+];
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // status= deactive => chi deactive thoi
+  // status=all => trả hết
+  // mac dinh chỉ tra active
+  const status = req.query.status;
+
+  let returnUsers;
+
+  if(status == 'deactive') {
+    returnUsers = users.filter(u => u.active == false)
+  } else if(status == 'all')
+  {
+    returnUsers = users
+  }
+  else {
+    returnUsers = users.filter(u => u.active == true)
+  }
+  res.json({users: returnUsers});
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const user = users.find(u => u.id == id);
+  res.json({'user': user})
+})
+
+router.post('/', (req, res) => {
+  const newUser = {
+    id: new Date().getTime(),
+    name: req.body.name,
+    active: req.body.active == 'true' ? true : false
+  }
+  users.push(newUser)
+  res.json(newUser)
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const active = req.body.active
+  res.json({id: id, active: active})
+})
+
+module.exports = router;
