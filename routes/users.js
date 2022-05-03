c27889d3fd7357db82190d97bb750abe68461e7b
hoangnhut <hoangnhut@evolgroup.vn>
2022-05-03 11:16:49 +0700

    Prettier format

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 let users = [
-  {id: 1, name: 'Alice', active: true},
-  {id: 2, name: 'Bob', active: true},
-  {id: 3, name: 'Charlie', active: false},
-  {id: 4, name: 'Dave', active: true},
-  {id: 5, name: 'Eve', active: true}
+  { id: 1, name: "Alice", active: true },
+  { id: 2, name: "Bob", active: true },
+  { id: 3, name: "Charlie", active: false },
+  { id: 4, name: "Dave", active: true },
+  { id: 5, name: "Eve", active: true },
 ];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   // status= deactive => chi deactive thoi
   // status=all => trả hết
   // mac dinh chỉ tra active
@@ -18,38 +18,36 @@ router.get('/', function(req, res, next) {
 
   let returnUsers;
 
-  if(status == 'deactive') {
-    returnUsers = users.filter(u => u.active == false)
-  } else if(status == 'all')
-  {
-    returnUsers = users
+  if (status == "deactive") {
+    returnUsers = users.filter((u) => u.active == false);
+  } else if (status == "all") {
+    returnUsers = users;
+  } else {
+    returnUsers = users.filter((u) => u.active == true);
   }
-  else {
-    returnUsers = users.filter(u => u.active == true)
-  }
-  res.json({users: returnUsers});
+  res.json({ users: returnUsers });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   const id = req.params.id;
-  const user = users.find(u => u.id == id);
-  res.json({'user': user})
-})
+  const user = users.find((u) => u.id == id);
+  res.json({ user: user });
+});
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const newUser = {
     id: new Date().getTime(),
     name: req.body.name,
-    active: req.body.active == 'true' ? true : false
-  }
-  users.push(newUser)
-  res.json(newUser)
-})
+    active: req.body.active == "true" ? true : false,
+  };
+  users.push(newUser);
+  res.json(newUser);
+});
 
-router.put('/:id', (req, res) => {
-  const id = req.params.id
-  const active = req.body.active
-  res.json({id: id, active: active})
-})
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const active = req.body.active;
+  res.json({ id: id, active: active });
+});
 
 module.exports = router;
