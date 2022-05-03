a599c003b215f19a7fa7f637da3c0b090b651100
hoangnhut <hoangnhut@evolgroup.vn>
2022-05-03 12:08:36 +0700

    Separate mock users data and init auth router

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,28 @@
 var express = require("express");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
 var router = express.Router();
+
+var users = require("../db/users");
+
+passport.use(
+  new LocalStrategy(function verify(username, password, callback) {
+    // sai username hoac password
+    return callback(null, false, {
+      message: "Incorrect username or password.",
+    });
+
+    // dung username hoac password
+    return callback(null, row);
+  })
+);
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/",
+  })
+);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,6 @@
 var express = require("express");
 var router = express.Router();
-
-let users = [
-  { id: 1, name: "Alice", active: true },
-  { id: 2, name: "Bob", active: true },
-  { id: 3, name: "Charlie", active: false },
-  { id: 4, name: "Dave", active: true },
-  { id: 5, name: "Eve", active: true },
-];
+var users = require('../db/users')
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
