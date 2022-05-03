9d8359e3277544fdf64dc42763ac7e121f98c73e
hoangnhut <hoangnhut@evolgroup.vn>
2022-05-03 16:22:45 +0700

    session authentication by passport

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var passport = require("passport");
+var session = require("express-session");
+var SQLiteStore = require("connect-sqlite3")(session);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var authRouter = require("./routes/auth");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/', authRouter);
-app.use('/users', usersRouter);
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new SQLiteStore({ db: "sessions.db", dir: "./db" }),
+  })
+);
+app.use(passport.authenticate("session"));
+
+passport.serializeUser(function (user, callback) {
+  process.nextTick(function () {
+    callback(null, { id: user.id, username: user.username });
+  });
+});
+
+passport.deserializeUser(function (user, callback) {
+  process.nextTick(function () {
+    return callback(null, user);
+  });
+});
+
+app.use("/", indexRouter);
+app.use("/", authRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,34 @@
 var express = require("express");
-var passport = require('passport');
-var LocalStrategy = require('passport-local');
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var users = require("../data/users");
 
 var router = express.Router();
 
-    passport.use(new LocalStrategy(function verify(username, password, callback) {
-        
-        if(?????)
-        {
-            // sai username+password
-            return callback(null, false, { message: 'Incorrect username or password.' });
-        }
-        
-        // dung username+password
-        return callback(null, user);
+passport.use(
+  new LocalStrategy(function verify(username, password, callback) {
+    const loggedInUser = users.find(
+      (u) => u.username === username && u.password === password
+    );
 
-    }));
+    if (!loggedInUser) {
+      // sai username+password
+      return callback(null, false, {
+        message: "Incorrect username or password.",
+      });
+    }
 
-router.post("/login", function (req, res, next) {
+    // dung username+password
+    return callback(null, loggedInUser);
+  })
+);
 
-  res.json(req.body);
-});
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/dasboard",
+    failureRedirect: "/",
+  })
+);
 
 module.exports = router;
